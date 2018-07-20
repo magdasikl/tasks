69cb81bab33c4f7f23a9a0ae7cc61f9568b5eb5d
magdasikl <msiklucka@gmail.com>
2018-07-20 20:00:17 +0200

    Zadanie 17.4

---
+++ showtasks.bat
@@ -0,0 +1,19 @@
+@echo off
+
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runbrowser
+echo.
+echo runcrud.bat has errors - breaking work
+goto fail
+
+:runbrowser
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080
+goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
