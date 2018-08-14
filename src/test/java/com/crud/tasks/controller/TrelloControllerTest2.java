5f3de88ebbdce5de1c81ea290940d411cba63c99
magdasikl <msiklucka@gmail.com>
2018-08-14 14:44:17 +0200

    Zadanie 21.2

--- src/test/java/com/crud/tasks/controller/TrelloControllerTest2.java
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest2.java
@@ -1,14 +1,14 @@
 package com.crud.tasks.controller;
 
-
-
-import com.crud.tasks.domain.TrelloBoardDto;
-import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.domain.*;
 import com.crud.tasks.trello.facade.TrelloFacade;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+//import org.mockito.Mock;
+//import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,21 +19,25 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+
 import static org.mockito.Mockito.when;
-
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+//@SpringBootTest
+//@RunWith(MockitoJUnitRunner.class)
 @RunWith(SpringRunner.class)
 @WebMvcTest(TrelloController.class)
-public class TrelloControllerTest {
+public class TrelloControllerTest2 {
+//    @Mock
+//    private TrelloController trelloController;
+//    @Autowired
+//    private TrelloService trelloService;
     @Autowired
     private MockMvc mockMvc;
     @MockBean
     private TrelloFacade trelloFacade;
+
 
     @Test
     public void shouldFetchEmptyTrelloBoards() throws Exception {
@@ -69,4 +73,63 @@ public class TrelloControllerTest {
                 .andExpect(jsonPath("$[0].lists[0].name", is("Test List")))
                 .andExpect(jsonPath("$[0].lists[0].closed", is(false)));
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @Test
+//    public void getTrelloBoards() {
+//        //Given
+//        List<TrelloListDto> trelloListDto = new ArrayList<>();
+//        trelloListDto.add(new TrelloListDto("1", "list1", false));
+//
+//        List<TrelloBoardDto> trelloBoardDto1 = new ArrayList<>();
+//        trelloBoardDto1.add(new TrelloBoardDto("1", "board1", trelloListDto));
+//
+//        when(trelloController.getTrelloBoards()).thenReturn(trelloBoardDto1);
+//        //When
+//        List<TrelloBoardDto> trelloBoardDtos = trelloController.getTrelloBoards();
+//        System.out.println(trelloBoardDtos);
+//        //Then
+//        trelloBoardDtos.forEach(trelloBoardDto -> {
+//            assertEquals("1", trelloBoardDto.getId());
+//            assertEquals("board1", trelloBoardDto.getName());
+//
+//            trelloBoardDto.getLists().forEach(trelloListDtos -> {
+//                assertEquals("1", trelloListDtos.getId());
+//                assertEquals("list1", trelloListDtos.getName());
+//                assertEquals(false, trelloListDtos.isClosed());
+//            });
+//
+//        });
+//
+//   }
+//
+//    @Test
+//    public void createdTrelloCard() {
+//        //Given
+//        CreatedTrelloCardDto createdTrelloCardDto = new CreatedTrelloCardDto("id",new TrelloBadgesDto(),"name","uu");
+//        TrelloCardDto trelloCardDto = new TrelloCardDto("card", "descr.card", "1", "1");
+//
+//        when(trelloController.createdTrelloCard(trelloCardDto)).thenReturn(createdTrelloCardDto);
+//
+//        //when
+//        CreatedTrelloCardDto createdTrelloCardDtos = trelloController.createdTrelloCard(trelloCardDto);
+//        //Then
+//        assertEquals("name",createdTrelloCardDtos.getName());
+//    }
 }
